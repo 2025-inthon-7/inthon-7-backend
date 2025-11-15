0e0e00d57f4b0e25ff1c2507b350a31933b019d6
hansung-ku <hansungk@korea.ac.kr>
2025-11-16 06:29:32 +0900

    debug

--- inthon7/asgi.py
+++ inthon7/asgi.py
@@ -1,16 +1,17 @@
 import os
+import django
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inthon7.settings")
+django.setup()
+
 import lecture.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inthon7.settings')
-
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(lecture.routing.websocket_urlpatterns)
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(lecture.routing.websocket_urlpatterns)),
+    }
+)
